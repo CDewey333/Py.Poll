70f785bec087d0e1c37271ce44c7fb47b5e12438
CDewey333 <104873544+CDewey333@users.noreply.github.com>
2022-05-30 13:16:16 -0600

    Add files via upload

---
+++ Election Analysis/Integers.py
@@ -0,0 +1,7 @@
+from unittest.util import _count_diff_all_purpose
+
+
+counties   
+{'Arapahoe', 'Denver', 'Jefferson'}
+counties{0}
+>>>print(counties{2})
---
+++ Election Analysis/Untitled-1.py
@@ -0,0 +1,38 @@
+from signal import default_int_handler
+
+
+    counties
+{'Arapahoe','Denver','Jefferson'}
+    counties{0}
+my_tuple = tuple()
+counties_tuple = ("Arapahoe","Denver","JEfferson")
+     len(counties_tuple)
+counties_tuple{1}
+{key:value}
+{key1:value1, key2:value2}
+     counties_dict = {"Arapahoe"} = 422829
+    counties_dict
+    counties_dict{"Denver"} = 463353
+    counties_dict{"Jefferson"} = 432438
+    en(counties_dict)
+    counties_dict.items()
+    counties_dict.keys()
+    counties_dict.values()
+    counties_dict{Arapahoe}
+TRaceback (most recent call last):
+File "<stdin>", line 1, in <module>
+NameError: name 'Arapahoe' is not defined
+    voting_data = {}
+# How many votes did you get?
+my_votes = int(input("How many votes did you get in the election? "))
+#  Total votes in the election
+total_votes = int(input("What is the total votes in the election? "))
+# Calculate the percentage of votes you received.
+percentage_votes = (my_votes / total_votes) * 100
+print("I received " + str(percentage_votes)+"% of the total votes.")
+temperature = int(input("What is the temperature outside?"))
+if temperature >80:
+    print("Turn on the AC.")
+else: 
+    print("Open the windows.")
+    
